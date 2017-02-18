69beb5aa0bf62f9440fd8991c775b1a7c7d8401a
Miguel <diazmiiguel@gmail.com>
2017-02-18 18:52:14 -0400

    Add some conditionals because data on web page changes, spaghetti, discord formatting<

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ bot.command :player do |event|
     page = mechanize.get("https://secure.tibia.com/community/?subtopic=characters&name=#{player_name}")
 
     # Yes, these are all pushes to an array so I don't gate rate limited on discord and this way I can
-    # Format the code nicely :-)
+    # Format the message nicely :-)
     # These are all xPaths from the Tibia website.
     message.push("Name: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[2]/td[2]').text}")
     message.push("Sex: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[3]/td[2]').text}")
@@ -25,24 +25,31 @@ bot.command :player do |event|
     message.push("Level: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[5]/td[2]').text}")
     message.push("World: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[7]/td[2]').text}")
     message.push("Residence: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[8]/td[2]').text}")
-    message.push("Last login: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[9]/td[2]').text}")
+    message.push("Last_login: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[9]/td[2]').text}")
 
   rescue NoMethodError
-    'Couldn\'t find data, maybe check your spelling?'
+    'No data'
   end
+    # FORMATTING AND NESTED IFs BECAUSE DATA FROM THE WEB PAGE CHANGES IF THE PLAYER DECIDES SO
+    # WEB SCRAP AIN'T EZ
 
     if !message.empty?
-      " ```
-      #{message[0]}
-      #{message[1]}
-      #{message[2]}
-      #{message[3]}
-      #{message[4]}
-      #{message[5]}
-      #{message[6]}
-      ``` "
+      message_to_send = " ```Ruby
+#{message[0]}
+#{message[1]}
+#{message[2]}
+#{message[3]}
+#{message[4]}
+#{message[5]}"
+      if !message[6].nil?
+        message_to_send += "
+#{message[6] unless message[6][12..-1].match(/\bCET\b/).nil?}```"
+      else
+        message_to_send += '```'
+      end
+
     else
-      'Couldn\'t find data, maybe check player\'s name?'
+      '**Couldn\'t find data, maybe check player\'s name?**'
     end
 end
 

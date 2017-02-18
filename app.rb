ed8a97b15787da9b5bcdf483b23ecc4cec87b9c0
Miguel <diazmiiguel@gmail.com>
2017-02-18 18:15:46 -0400

    Deleted some lines and creation date to make code kinda more friendly

--- app.rb
+++ app.rb
@@ -11,11 +11,14 @@ bot.command :player do |event|
   content = event.message.content.split(' ')
   player_name = content[1]
   message = []
-  except = []
 
   begin
+    # name from the message sent to the bot
     page = mechanize.get("https://secure.tibia.com/community/?subtopic=characters&name=#{player_name}")
 
+    # Yes, these are all pushes to an array so I don't gate rate limited on discord and this way I can
+    # Format the code nicely :-)
+    # These are all xPaths from the Tibia website.
     message.push("Name: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[2]/td[2]').text}")
     message.push("Sex: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[3]/td[2]').text}")
     message.push("Vocation: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[4]/td[2]').text}")
@@ -23,27 +26,24 @@ bot.command :player do |event|
     message.push("World: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[7]/td[2]').text}")
     message.push("Residence: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[8]/td[2]').text}")
     message.push("Last login: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[9]/td[2]').text}")
-    message.push("Created: #{page.at('//*[@id="characters"]/div[5]/div/div/table[3]/tr[3]/td[2]').text}")
 
   rescue NoMethodError
-    except.push('Creation date not found') #Just did this because otherwise it would be sent to the server
-  rescue
+    'Couldn\'t find data, maybe check your spelling?'
   end
 
-  if message[7].nil?
-    message_to_send =
-    " ```
-    #{message[0]}
-    #{message[1]}
-    #{message[2]}
-    #{message[3]}
-    #{message[4]}
-    #{message[5]}
-    #{message[6]}
-    ``` "
-  else
-    message_to_send += "``` #{message[8]} ```"
-  end
+    if !message.empty?
+      " ```
+      #{message[0]}
+      #{message[1]}
+      #{message[2]}
+      #{message[3]}
+      #{message[4]}
+      #{message[5]}
+      #{message[6]}
+      ``` "
+    else
+      'Couldn\'t find data, maybe check player\'s name?'
+    end
 end
 
 bot.run

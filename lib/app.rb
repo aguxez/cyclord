7ec8528257797212ba05058d7d206d6aafa175f2
Miguel <diazmiiguel@gmail.com>
2017-02-20 14:05:39 -0400

    Changes conditionals for embed message, looks prettier

--- lib/app.rb
+++ lib/app.rb
@@ -19,49 +19,38 @@ bot.command :player do |event|
   player_name = content[1..-1].join('+')
   player_name_pm = content[1..-1].map(&:capitalize).join(' ')
   message = []
+  colors = %w(#FC4E29 #55D511 #1298DF #3523B4 #D678EB)
+  embed_colors = rand(1...colors.length + 1)
 
   begin
     # name from the message sent to the bot
     page = mechanize.get("https://secure.tibia.com/community/?subtopic=characters&name=#{player_name}")
     shorten = Bitly.client.shorten("https://secure.tibia.com/community/?subtopic=characters&name=#{player_name}")
-    # Sends Shortened link to User as a Private Message because you can't send clickable links on Markdown
-    event.user.pm("Full profile of **#{player_name_pm}**: #{shorten.short_url}")
 
     # Yes, these are all pushes to an array so I don't gate rate limited on discord and this way I can
     # Format the message nicely :-)
     # These are all xPaths from the Tibia website.
-    message.push("Name: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[2]/td[2]').text}")
-    message.push("Sex: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[3]/td[2]').text}")
-    message.push("Vocation: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[4]/td[2]').text}")
-    message.push("Level: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[5]/td[2]').text}")
-    message.push("World: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[7]/td[2]').text}")
-    message.push("Residence: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[8]/td[2]').text}")
-    message.push("Last_login: #{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[9]/td[2]').text}")
-    message.push("Full_profile: #{shorten.short_url} (Sent on PM)")
+    message.push("#{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[2]/td[2]').text}")
+    message.push("#{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[3]/td[2]').text}")
+    message.push("#{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[4]/td[2]').text}")
+    message.push("#{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[5]/td[2]').text}")
+    message.push("#{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[7]/td[2]').text}")
+    message.push("#{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[8]/td[2]').text}")
+    message.push("#{page.at('//*[@id="characters"]/div[5]/div/div/table[1]/tr[9]/td[2]').text}")
 
   rescue NoMethodError
     'No data'
   end
-    # FORMATTING AND NESTED IFs BECAUSE DATA FROM THE WEB PAGE CHANGES IF THE PLAYER DECIDES SO
-    # WEB SCRAP AIN'T EZ
-
+    # CHANGED IFS STATEMENTS FOR EMBEDDED MESSAGE, LOOKS NICER AND HAS LINKS
     if !message.empty?
-      message_to_send = " ```Ruby
-#{message[0]}
-#{message[1]}
-#{message[2]}
-#{message[3]}
-#{message[4]}
-#{message[5]}
-#{message[7]}"
-
-      if !message[6].nil?
-        message_to_send += "
-#{message[6] unless message[6][12..-1].match(/\bCET\b/).nil?}```"
-      else
-        message_to_send += '```'
+      event.channel.send_embed do |embed|
+        embed.message = { text: "**#{message[0]}**, #{message[1]} #{message[2]}, **level #{message[3]}**
+        from **#{message[4]}** and resides in **#{message[5]}**" }
+        embed.color = colors[embed_colors]
+        embed.author = { name: player_name_pm, icon_url: event.user.avatar_url}
+        embed.url = shorten.short_url
+        embed.thumbnail = { url: event.user.avatar_url }
       end
-
     else
       '**Couldn\'t find data, maybe check player\'s name?**'
     end
